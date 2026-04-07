b40f2d537803ed397f097fde5998a6a4df0ac1ef
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-07 15:07:52 -0400

    Question 1

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+        // Question 1
+        double bobSalary = 30000;
+        double garySalary = 52000;
+        double highestSalary = Math.max(bobSalary, garySalary);
+
+        System.out.println("The highest salary is " + highestSalary + ".");
+
+    }
 }
